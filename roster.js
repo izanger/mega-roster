606cba279ec02e963c320fe80253fe36bb4370fb
Ian Zanger <izanger@purdue.edu>
2017-05-16 14:51:10 -0400

    Add promote button that bolds and outlines

--- roster.js
+++ roster.js
@@ -6,16 +6,36 @@ const handleSubmit = (ev) => {
     const list = document.getElementById('ulList')
     const name = form.personName.value
 
+
+const promoteButton = document.createElement('button')
+    promoteButton.type = "submit"
+    promoteButton.textContent = "Promote"
+    list.insertBefore(promoteButton, list.firstChild)
+    promoteButton.addEventListener('click', function (){
+        let listing = this.previousElementSibling.previousElementSibling
+        listing.style.padding = "3px 3px 3px 5px"
+        listing.style.width = listing.textContent.length * 8 + "px"
+        listing.style.borderStyle = "solid"
+        listing.style.fontWeight = "bold"
+        
+
+    })
+
+
     const delButton = document.createElement('button')
     delButton.type = "submit"
     delButton.textContent = "Delete"
+    delButton.display = "inline"
     list.insertBefore(delButton, list.firstChild)
     delButton.addEventListener('click', function (){
        //'this' is delButton
+       this.nextElementSibling.remove()
        this.previousElementSibling.remove()
        this.remove()
 
     })
+
+    
 
     const element = document.createElement('li')
     element.textContent = name
